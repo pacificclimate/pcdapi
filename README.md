84ac0a85b0e50a0de8ae6f42a354b41cfa2dcb20
Basil Veerman <basilveerman@gmail.com>
2016-06-23 08:43:00 -0700

    Add some package front matter

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# pcds-backend
+# pcdapi
+
+## Installation
+
+```bash
+pyvenv venv
+$ source venv/bin/activate
+(venv)$ pip install -U pip
+(venv)$ pip install --trusted-host tools.pacificclimate.org -i http://tools.pacificclimate.org/pypiserver/ -e .
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.0.1'
+
+setup(
+    name="pcdapi",
+    description="PCIC's PCDS backend API",
+    keywords="science climate meteorology weather",
+    packages=find_packages(),
+    version=__version__,
+    url="http://www.pacificclimate.org/",
+    author="James Hiebert <hiebert@uvic.ca>, Basil Veerman <bveerman@uvic.ca>",
+    author_email="climate@uvic.ca",
+    # install_requires = [
+    #     'flask',
+    #     'Flask-SQLAlchemy',
+    #     'modelmeta'
+    # ],
+    # scripts = ['scripts/devserver.py'],
+    package_dir = {'pcdapi': 'pcdapi'},
+    zip_safe=False
+)
